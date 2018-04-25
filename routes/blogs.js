de3a95011a2bb0c3570b66497c208ff83d9f227e
Samuel Krescanko <samoo41@gmail.com>
2018-04-26 00:07:17 +0200

    edit/delete blog buttons are now hidden from a unauthorized user

--- routes/blogs.js
+++ routes/blogs.js
@@ -58,22 +58,14 @@ router.get("/:id/edit",checkBlogOwnership, function(req,res){
 // UPDATE BLOG ROUTE
 router.put("/:id",checkBlogOwnership, function(req,res){
 	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err,updatedBlog){
-		if(err){
-			res.redirect("/blogs");
-		} else {
-			res.redirect("/blogs/" + req.params.id);
-		}
+		res.redirect("/blogs/" + req.params.id);
 	})
 })
 
 // DELETE BLOG ROUTE
 router.delete("/:id",checkBlogOwnership, function(req,res){
 	Blog.findByIdAndRemove(req.params.id, function(err){
-		if(err){
-			res.redirect("/blogs");
-		} else {
-			res.redirect("/blogs");
-		}
+		res.redirect("/blogs");
 	})
 })
 
